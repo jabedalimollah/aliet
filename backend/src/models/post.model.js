1e547b2f043383f7e2cf458f434b69a30c9c0ce7
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2024-11-04 16:25:42 +0530

    add user, post and comment model

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const postSchema = new mongoose.Schema({
+  caption: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  auther: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+const Post = mongoose.model("Post", postSchema);
+export { Post };
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export { User };
