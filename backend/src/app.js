5f577757ac7f0a481495ebf8de745dbdf511d499
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2024-11-04 16:24:39 +0530

    add database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// ----------- Middlewares ----------
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 export { app };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 import dotnev from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/db.js";
 dotnev.config();
 
+// ------------ PORT -----------
 const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server runnig on Port ${PORT}`);
-});
+// --------- Database Connection ---------
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed !!!", err);
+  });
