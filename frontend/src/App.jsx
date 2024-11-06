8109df25cead58286503cf9acc98f8c55eb15d0c
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2024-11-06 16:21:17 +0530

    add initial files for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Signup from "./components/Signup";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Signup />
+    </>
+  );
+}
+
+export default App;
